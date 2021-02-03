e92a30494509632086677d082136e2b03952af78
horizon <1820120473@qq.com>
2021-02-04 02:23:13 +0800

    优化下

--- engine.py
+++ engine.py
@@ -3,7 +3,7 @@ from typing import Set, Iterable, Any
 from tcod.context import Context
 from tcod.console import Console
 
-from actions import EscapeAction, MovementAction
+
 from entity import Entity
 from game_map import GameMap
 from input_handlers import EventHandler
@@ -24,14 +24,9 @@ class Engine:
 
             if action is None:
                 continue
-
-            if isinstance(action, MovementAction):
-                if self.game_map.tiles["walkable"][self.player.x + action.dx, self.player.y + action.dy] :
-                    self.player.move(dx=action.dx, dy=action.dy)
-
-
-            elif isinstance(action, EscapeAction):
-                raise SystemExit()
+            
+            # 消息捕获
+            action.perform(self, self.player)
 
 
     # 渲染方法    
