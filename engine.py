08522a6dec74037db77363ee9b961b5d879480e0
horizon <1820120473@qq.com>
2021-02-04 02:11:38 +0800

    生成地图

--- engine.py
+++ engine.py
@@ -5,14 +5,16 @@ from tcod.console import Console
 
 from actions import EscapeAction, MovementAction
 from entity import Entity
+from game_map import GameMap
 from input_handlers import EventHandler
 
 # 游戏引擎
 class Engine:
     # 初始化
-    def __init__(self, entities: Set[Entity], event_handler: EventHandler, player: Entity):
+    def __init__(self, entities: Set[Entity], event_handler: EventHandler, game_map: GameMap, player: Entity):
         self.entities = entities
         self.event_handler = event_handler
+        self.game_map = game_map
         self.player = player
 
     # 事件方法
@@ -24,16 +26,20 @@ class Engine:
                 continue
 
             if isinstance(action, MovementAction):
-                self.player.move(dx=action.dx, dy=action.dy)
+                if self.game_map.tiles["walkable"][self.player.x + action.dx, self.player.y + action.dy] :
+                    self.player.move(dx=action.dx, dy=action.dy)
+
+
             elif isinstance(action, EscapeAction):
                 raise SystemExit()
 
 
     # 渲染方法    
     def render(self, console: Console, context: Context) -> None:
+        self.game_map.render(console)
+
         for entity in self.entities:
-            console.print(x=entity.x, y=entity.y,
-                          string=entity.char, fg=entity.color)
+            console.print(x=entity.x, y=entity.y, string=entity.char, fg=entity.color)
 
         # 渲染控制台
         context.present(console)
